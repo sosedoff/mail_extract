8950bfc46432b463d475d2e38535164915a63456
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-08-12 13:48:13 -0500

    detect iphone/ipad signatures

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -15,6 +15,11 @@ describe 'MailExtract::Parser' do
     body = MailExtract.new(fixture('reply_with_quotes.txt')).body
     body.should == 'This is a first line of the message'
   end
+  
+  it 'parses a message send via iphone' do
+    body = MailExtract.new(fixture('iphone.txt')).body
+    body.should == 'This is a shit i sent from my iphone'
+  end
 
   it 'parses a reply sent via iphone' do
     body = MailExtract.new(fixture('iphone_with_quotes.txt'), :only_head => true).body
